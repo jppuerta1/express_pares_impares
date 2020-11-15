24ad6d06f0ce7c20a592168c8d9b60bdcd09a914
jppuerta1 <jppuertaorrego@gmail.com>
2020-11-15 15:23:35 -0800

    quinto commit

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
  
 var app = express();
+
+let num=[];
+for(let i=1;i<=50;i++){
+    if(i%2==0){
+        num[i]=i+' Soy par!'
+    }
+    else{
+
+        num[i]=i+' Soy Impar!'
+    }
+}
  
 app.engine('handlebars',exphbs());
 app.set('view engine', 'handlebars');
  
 app.get('/',(req, res) =>{
-    res.render('home');
+    res.render('home',{
+        num
+    });
 });
  
 app.listen(3000, console.log('puerto 3000'));
